266674961e37297c669875a1e1c57059c581e24f
SergeiTheVodker <sargedines@gmail.com>
2024-09-07 13:01:31 +0800

    Add setter and getter for EmployeeResponseDto

--- src/main/java/com/fullstacktest/crudapp/dto/EmployeeResponseDto.java
+++ src/main/java/com/fullstacktest/crudapp/dto/EmployeeResponseDto.java
@@ -1,7 +1,11 @@
 package com.fullstacktest.crudapp.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @AllArgsConstructor
 public class EmployeeResponseDto {
     Long id;
