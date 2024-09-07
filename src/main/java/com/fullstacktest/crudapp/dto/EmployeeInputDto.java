ee70fd8cc0678bdfca6eb5f2e6ce65d87993b9c9
SergeiTheVodker <sargedines@gmail.com>
2024-09-07 13:01:18 +0800

    Remove unused import.

--- src/main/java/com/fullstacktest/crudapp/dto/EmployeeInputDto.java
+++ src/main/java/com/fullstacktest/crudapp/dto/EmployeeInputDto.java
@@ -1,7 +1,6 @@
 package com.fullstacktest.crudapp.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fullstacktest.crudapp.enums.Gender;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
